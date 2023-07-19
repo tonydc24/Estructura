c4beec06722dc1fb80ed92eaca614ba3d5a41e9f
tonydc24 <anthonydiazc09@gmail.com>
2023-07-19 00:00:08 -0600

    Constructor , getters and setters

--- src/estructura_proyecto/Comida.java
+++ src/estructura_proyecto/Comida.java
@@ -14,4 +14,43 @@ private int Carne;
 private int Queso;
 private int lechuga;
 
+    public Comida(int pan, int Carne, int Queso, int lechuga) {
+        this.pan = pan;
+        this.Carne = Carne;
+        this.Queso = Queso;
+        this.lechuga = lechuga;
+    }
+
+    public int getPan() {
+        return pan;
+    }
+
+    public void setPan(int pan) {
+        this.pan = pan;
+    }
+
+    public int getCarne() {
+        return Carne;
+    }
+
+    public void setCarne(int Carne) {
+        this.Carne = Carne;
+    }
+
+    public int getQueso() {
+        return Queso;
+    }
+
+    public void setQueso(int Queso) {
+        this.Queso = Queso;
+    }
+
+    public int getLechuga() {
+        return lechuga;
+    }
+
+    public void setLechuga(int lechuga) {
+        this.lechuga = lechuga;
+    }
+    
 }
