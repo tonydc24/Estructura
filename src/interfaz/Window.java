de792fcc70107f82ce41ef7bdc44f6422b5d0e2a
antho <antho@Tony>
2023-07-30 14:42:32 -0600

    Comentarios

--- src/interfaz/Window.java
+++ src/interfaz/Window.java
@@ -6,6 +6,8 @@ package interfaz;
 
 import java.awt.Canvas;
 import java.awt.Dimension;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
 
 /**
@@ -17,14 +19,15 @@ public class Window extends JFrame implements Runnable {
     public static final int width = 800, height = 600;
     private Canvas canvas;
     private Thread thread;
-
+    private boolean running=false;
     public Window() {
         setTitle("Juego");
         setSize(width, height);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(false);
-        setLocationRelativeTo(null);
-
+        setResizable(false);//No permite cambiar el size de la ventana
+        setLocationRelativeTo(null);//Inicia la ventana en  medio del screen
+        //Agrego funciones basicas como el ancho y la altura ,
+        //la opcion de salir de la ventana 
         setVisible(true);
 
         canvas = new Canvas();
@@ -37,19 +40,33 @@ public class Window extends JFrame implements Runnable {
     }
 
     public static void main(String[] args) {
-        new Window();
+        new Window().start();
 
     }
 
     @Override
     public void run() {
+        while (running){
+            
+        
+        }
+        stop();
 
     }
 
     private void start() {
-
+        thread = new Thread(this);
+        thread.start();//Inicia el hilo run()
+        running = true;//Permite iniciar el booleano que inicia el juego
     }
 
     private void stop() {
+        try {
+            thread.join();
+            running = false ;//Cierra el booleano para cerrar el juego
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Window.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        }
     }
 }
