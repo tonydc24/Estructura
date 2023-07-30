02e53a120afff78347179580f90e84437617de89
antho <antho@Tony>
2023-07-30 14:35:21 -0600

    Hilos

--- src/interfaz/Window.java
+++ src/interfaz/Window.java
@@ -5,16 +5,19 @@
 package interfaz;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
 import javax.swing.JFrame;
 
 /**
  *
  * @author antho
  */
-public class Window extends JFrame {
-   
+public class Window extends JFrame implements Runnable {
+
     public static final int width = 800, height = 600;
     private Canvas canvas;
+    private Thread thread;
+
     public Window() {
         setTitle("Juego");
         setSize(width, height);
@@ -23,10 +26,30 @@ public class Window extends JFrame {
         setLocationRelativeTo(null);
 
         setVisible(true);
+
+        canvas = new Canvas();
+        canvas.setPreferredSize(new Dimension(width, height));
+        canvas.setMaximumSize(new Dimension(width, height));
+        canvas.setMinimumSize(new Dimension(width, height));
+        canvas.setFocusable(true);
+
+        add(canvas);
     }
 
     public static void main(String[] args) {
         new Window();
 
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    private void start() {
+
+    }
+
+    private void stop() {
     }
 }
