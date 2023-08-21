4017377782d6b4bc4d29521997d019293b2b00b5
Lidietholivar98 <lolivar40908@ufide.ac.cr>
2023-08-21 00:24:44 -0600

    Se agrega logica para determinar si una orden procesada es valida y devuelve los puntos, de lo contrario, devuelve 0

--- src/MisClases/OrdenControl.java
+++ src/MisClases/OrdenControl.java
@@ -6,6 +6,7 @@ package MisClases;
 
 import Estructura.Cola;
 import Estructura.ListaMesa;
+import Estructura.NodoHamburguesa;
 import java.awt.Graphics;
 import java.util.Random;
 
@@ -13,8 +14,9 @@ public class OrdenControl {
 
     private Cola colaDeOrdenes;
     private ListaMesa listaMesa;
-    private int salto ; 
+    private int salto;
     private int inicial;
+
     public OrdenControl() {
         colaDeOrdenes = new Cola();
     }
@@ -22,28 +24,37 @@ public class OrdenControl {
     public void generarOrdenAleatoria() {
         int tipoDeHamburguesa = getRandomNumber();
         if (tipoDeHamburguesa == 1) {
-            Orden orden = new Orden("Hamburguesa de carne", 5,2,12);
+            Orden orden = new Orden("Hamburguesa de carne", 5, 2, 12);
             colaDeOrdenes.inserta(orden);
         }
         if (tipoDeHamburguesa == 2) {
-            Orden orden = new Orden("Hamburguesa con queso", 10,3,23);
+            Orden orden = new Orden("Hamburguesa con queso", 10, 3, 23);
             colaDeOrdenes.inserta(orden);
         }
         if (tipoDeHamburguesa == 3) {
-            Orden orden = new Orden("Hamburguesa clasica", 15,4,32);
+            Orden orden = new Orden("Hamburguesa clasica", 15, 4, 32);
             colaDeOrdenes.inserta(orden);
         }
 
     }
-    public void ordenTerminada(){
-    
-    
-    
+
+    public int ordenTerminada() {
+        int puntosObtenidos = 0;
+        NodoHamburguesa nodoOrden = colaDeOrdenes.elimina();
+        Orden ordenProcesada = nodoOrden.getOrden();
+        int especificacion = ordenProcesada.getEspecificar();
+        //recorrer la lista de la mesa para obtener la suma de los pesos de cada ingrediente
+        int sumaPesosIngredientesEnMesa = 0;
+        if (sumaPesosIngredientesEnMesa == especificacion) {
+            puntosObtenidos = ordenProcesada.getPuntos();
+        }
+        return puntosObtenidos;
     }
-    public void drawOrden(Graphics g){
-    colaDeOrdenes.drawOrdenes(g, salto, inicial);    
-    } 
-   
+
+    public void drawOrden(Graphics g) {
+        colaDeOrdenes.drawOrdenes(g, salto, inicial);
+    }
+
     public static int getRandomNumber() {
         Random random = new Random();
         return random.nextInt(3) + 1;
